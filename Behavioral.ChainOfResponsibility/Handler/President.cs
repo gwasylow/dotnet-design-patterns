a2064beb0bf0f8682e7a3fc3a71b7e5e6a3a1385
GregW <g.wasylow@gmail.com>
2019-11-14 14:20:18 +0100

    Behavioral.ChainOfResponsibility pattern & Readme update & DrawIO diagram

---
+++ Behavioral.ChainOfResponsibility/Handler/President.cs
@@ -0,0 +1,26 @@
+﻿using Behavioral.ChainOfResponsibility.AbstractHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavioral.ChainOfResponsibility.Handler
+{
+    /// <summary>
+    /// The 'ConcreteHandler' class
+    /// </summary>
+    class President : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 100000.0)
+                Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+            else
+            {
+                //End of a chain.
+                Console.WriteLine("President says: Request# {0} requires an executive meeting!", purchase.Number);
+            }
+        }
+    }
+}
