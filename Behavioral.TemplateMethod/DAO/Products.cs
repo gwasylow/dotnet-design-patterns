24f683e1be2ea412e167dbb2633a9b22def474f1
g.wasylow@gmail.com <g.wasylow@gmail.com>
2020-03-09 15:09:38 +0100

    Behavioral.TemplateMethod code source & REAMDE update & new diagram.

---
+++ Behavioral.TemplateMethod/DAO/Products.cs
@@ -0,0 +1,37 @@
+﻿using Behavioral.TemplateMethod.AbstractDAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Behavioral.TemplateMethod.DAO
+{
+    /// <summary>
+    /// A 'ConcreteClass' class
+    /// </summary>
+    public class Products : DataAccessObject
+    {
+        public override void Select()
+        {
+            string sql = "select ProductName from Products";
+            //OleDbDataAdapter is not a .NET CORE library
+            //OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, connectionString);
+
+            base.dataSet = new List<string>();
+            //dataAdapter.Fill(dataSet, "Products");
+        }
+
+        public override void Process()
+        {
+            Console.WriteLine("Products ---- ");
+            base.dataSet.Add("Wtyczka samochodowa");
+            base.dataSet.Add("Dezodorant");
+            base.dataSet.Add("Zegarek");
+
+            foreach (var row in base.dataSet)
+                Console.WriteLine(row);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Behavioral.TemplateMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using Behavioral.TemplateMethod.AbstractDAO;
+using Behavioral.TemplateMethod.DAO;
+using System;
+
+namespace Behavioral.TemplateMethod
+{
+    /**
+     *  AbstractClass  (DataObject)
+     *      - defines abstract primitive operations that concrete 
+     *          subclasses define to implement steps of an algorithm
+     *      - implements a template method defining the skeleton 
+     *          of an algorithm. The template method calls primitive 
+     *          operations as well as operations defined in AbstractClass 
+     *          or those of other objects.
+     *      
+     *  ConcreteClass  (CustomerDataObject)
+     *      - implements the primitive operations ot carry out 
+     *          subclass-specific steps of the algorithm
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccessObject daoCategories = new Categories();
+            daoCategories.Run();
+
+            DataAccessObject daoProducts = new Products();
+            daoProducts.Run();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
